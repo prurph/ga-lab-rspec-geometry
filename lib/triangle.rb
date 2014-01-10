b5bfd2475699023aac757091d7297479e6413672
Prescott Murphy <prurph@gmail.com>
2014-01-10 16:51:55 -0500

    Added find_cos method to simplify the angles method in triangle.rb

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -18,14 +18,17 @@ module Geometry
       (hp*(hp-@sideA)*(hp-@sideB)*(hp-@sideC)) ** 0.5
     end
 
+    def find_cos(a, b, c) # Returns cos of angle opposite c
+      (a.to_f ** 2 + b.to_f ** 2 - c.to_f ** 2) / (2 * a.to_f * b.to_f)
+    end
+
     def angles
       sides = [@sideA.to_f, @sideB.to_f, @sideC.to_f]
       angles = []
 
-      # put cos of each angle into angles
-      angles[0] = (sides[1] ** 2 + sides[2] ** 2 - sides[0] ** 2) / (2 * sides[1] * sides[2])
-      angles[1] = (sides[0] ** 2 + sides[2] ** 2 - sides[1] ** 2) / (2 * sides[0] * sides[2])
-      angles[2] = (sides[0] ** 2 + sides[1] ** 2 - sides[2] ** 2) / (2 * sides[0] * sides[1])
+      angles[0] = find_cos(@sideB, @sideC, @sideA)
+      angles[1] = find_cos(@sideC, @sideA, @sideB)
+      angles[2] = find_cos(@sideA, @sideB, @sideC)
 
       # now cos-1 the array and convert to degrees
       angles.map!{ |x| Math.acos(x) * 180 / Math::PI }
